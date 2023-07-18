062c80f0baae010ed568307f9a66034e06c3fb66
sadasiva111 <sadasiva6699@gmail.com>
2023-07-18 17:52:21 +0530

    Added LL Insert

--- LinkedList/main.java
+++ LinkedList/main.java
@@ -6,7 +6,7 @@ public class main {
         myLinkedList.appendNode(1);
         myLinkedList.appendNode(2);
         myLinkedList.appendNode(3);
-        System.out.println(myLinkedList.set(2, 4));
+        myLinkedList.insert(2, 8);
         myLinkedList.printList();
     }
 }
